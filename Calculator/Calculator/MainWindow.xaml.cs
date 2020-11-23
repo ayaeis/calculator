f7703ebaa03030aa96d99be14e68f90fe46a8ae6
ayaeis <54796973+ayaeis@users.noreply.github.com>
2020-11-23 19:55:04 +0100

    koden nästan klar

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -20,9 +20,61 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double  number1 = 0;
+        double number2 = 0;
+        string operation = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button button)
+            {
+                if (operation == "")
+                {
+                    number1 = (number1 * 10) + Convert.ToInt32(button.Content) ; 
+                    Display.Text = number1.ToString();
+                }
+                else
+                {
+                    number2 = (number2 * 10) + Convert.ToInt32(button.Content);
+                    Display.Text = number2.ToString();
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button button)
+            {
+                operation = button.Content.ToString();
+                Display.Text = "0";
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button button)
+            {
+                switch (operation)
+                {
+                    case "+":
+                        Display.Text = (number1 + number2).ToString();
+                        break;
+                    case "-":
+                        Display.Text = (number1 - number2).ToString();
+                        break;
+                    case "x":
+                        Display.Text = (number1 * number2).ToString();
+                        break;
+                    case "/":
+                        Display.Text = (number1 / number2).ToString();
+                        break;
+                }
+
+            }
         }
     }
 }
